0212d48978884d2734802d8d4cb1b443931bf7c0
Djaysambo <vicentesambo3@gmail.com>
2024-08-06 11:55:37 +0200

    feat: image folder

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,21 +2,14 @@ import React from "react";
 
 export const HomePage = () => {
   return (
-    <main
-      style={{
-        position: "relative",
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-        margin: 0,
-        padding: 0,
-        boxSizing: "border-box",
-      }}
+    <main className="relative w-full h-full overflow-hidden m-0 p-0 box-border"
+     
     >
       <video
         autoPlay
         muted
         loop
+        className=" w-screen h-full object-cover"
         style={{
           position: "absolute",
           top: 0,
@@ -43,33 +36,23 @@ export const HomePage = () => {
           overflow: "hidden",
         }}
       >
-        <div
+        <div 
+        className="p-8 text-center max-w-7xl w-1/4 box-border rounded-3xl "
           style={{
             backgroundColor: "rgba(255, 255, 255, 0.8)",
             padding: "1.25rem",
             borderRadius: "1.25rem",
             boxShadow: "0 0 0.625rem rgba(0, 0, 0, 0.3)",
-            textAlign: "center",
-            maxWidth: "90vw",
             width: "25%",
-            boxSizing: "border-box",
+           
           }}
         >
-          <h1
-            style={{
-              fontSize: "2rem",
-              fontWeight: "bold",
-              margin: 0,
-            }}
+          <h1 className="font-large-bold"
+            
           >
             HAPPY-PETS
           </h1>
-          <div
-            style={{
-              marginTop: "0.5rem",
-              fontSize: "1rem",
-              textAlign: "left",
-            }}
+          <div className="m-2 text-left  "
           >
             <p>
               Bem-vindo à nossa loja especializada na venda de rações, diversas
@@ -78,27 +61,13 @@ export const HomePage = () => {
               entender e aproveitar ao máximo os nossos produtos voltados para o
               bem-estar dos seus amigos de quatro patas.
             </p>
-            <p>
-              Bem-vindo à nossa loja especializada na venda de rações, diversas
-              raças de cachorros, e acessórios para cachorros! Aqui você
-              encontrará todas as informações necessárias para explorar,
-              entender e aproveitar ao máximo os nossos produtos voltados para o
-              bem-estar dos seus amigos de quatro patas. JOCK Grandeur é
-              formulado cientificamente para liberar a lenda interna do seu
-              cachorro. Usando ingredientes de qualidade, nossa ração pesquisada
-              por veterinários não é apenas deliciosa, mas também rica em
-              proteínas e carregada com minerais e vitaminas essenciais para
-              atender às necessidades diárias do seu cachorro.
-            </p>
+            
             <button
+            className="mt-4 cursor-pointer font-bold p-2 "
               style={{
                 backgroundColor: "#80ED99",
                 borderRadius: "1.25rem",
-                padding: "0.5rem 1rem",
-                cursor: "pointer",
-                fontSize: "1rem",
-                fontWeight: "bold",
-                marginTop: "1rem",
+                fontSize: "1rem",          
                 display: "inline-block",
               }}
             >
